eb06b8ff46fead909b13cf6423198749eab794c2
penhaMa <130068243+penhaMa@users.noreply.github.com>
2023-05-16 00:00:20 -0300

    Segundo commit

--- projetoFinanceiro/Program.cs
+++ projetoFinanceiro/Program.cs
@@ -1,46 +1,119 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Security.Cryptography;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace projetoFinanceiro
 {
-    internal class Program
+    class Program
     {
+        // Dicionário para armazenar os usuários e senhas
+        static Dictionary<string, string> usuarios = new Dictionary<string, string>();
+
         static void Main(string[] args)
         {
-            string usuarioLogin = "mar";
-            int usuarioSenha = 123;
+            bool verificar = false;
 
-            string login;
-            int senha;
-            int tentativas = 0;
-
-            do
+            while (!verificar)
             {
-                Console.Clear();
-                Console.WriteLine("Digite o Login: ");
-                login = Console.ReadLine();
+                Console.WriteLine("1. Login");
+                Console.WriteLine("2. Cadastrar novo usuário");
+                Console.Write("Escolha uma opção: ");
+                string opcao = Console.ReadLine();
 
-                Console.WriteLine("Digite a Senha: ");
-                senha = Convert.ToInt32(Console.ReadLine());
+                switch (opcao)
+                {
+                    case "1":
+                        FazerLogin();
+                        verificar = true;
+                        Escolha();
+                        break;
+                    case "2":
+                        CadastrarUsuario();
+                        break;
+                    default:
+                        Console.WriteLine("Opção inválida!");
+                        break;
+                }
 
-                tentativas++;
+                Console.WriteLine();
+            }
+        }
 
-            } while (login != usuarioLogin && senha != usuarioSenha && tentativas < 3);
+        static void FazerLogin()
+        {
+            Console.Write("Digite o login: ");
+            string inputLogin = Console.ReadLine();
 
-            if (login == usuarioLogin && senha == usuarioSenha)
+            Console.Write("Digite a senha: ");
+            string inputSenha = Console.ReadLine();
+
+            if (usuarios.ContainsKey(inputLogin) && VerificarSenha(inputSenha, usuarios[inputLogin]))
             {
-                Console.WriteLine("Seja bem vindo ao sistema!!");
+                Console.WriteLine("Login bem-sucedido!");
+                // Coloque o código adicional aqui para o que deseja fazer após o login.
             }
             else
             {
-                Console.Clear();
-                Console.WriteLine("Login ou senha incorretos, digite novamente: ");
+                Console.WriteLine("Login ou senha incorretos!");
+            }
+        }
+
+        static void CadastrarUsuario()
+        {
+            Console.Write("Digite o novo login: ");
+            string novoLogin = Console.ReadLine();
+
+            if (usuarios.ContainsKey(novoLogin))
+            {
+                Console.WriteLine("O login já está em uso!");
+                return;
             }
 
-            Console.ReadKey();
+            Console.Write("Digite a nova senha: ");
+            string novaSenha = Console.ReadLine();
+
+            // Gerar hash da senha
+            string senhaHash = GerarHashSenha(novaSenha);
+
+            // Armazenar login e senha hash no dicionário
+            usuarios[novoLogin] = senhaHash;
+
+            Console.WriteLine("Usuário cadastrado com sucesso!");
+        }
+
+        static string GerarHashSenha(string senha)
+        {
+            // Criar instância do algoritmo de hash SHA256
+            using (SHA256 sha256Hash = SHA256.Create())
+            {
+                // Converter a senha em bytes e calcular o hash
+                byte[] bytes = sha256Hash.ComputeHash(Encoding.UTF8.GetBytes(senha));
+
+                // Construir a representação em string do hash
+                StringBuilder builder = new StringBuilder();
+                for (int i = 0; i < bytes.Length; i++)
+                {
+                    builder.Append(bytes[i].ToString("x2"));
+                }
+
+                return builder.ToString();
+            }
+        }
+
+        static bool VerificarSenha(string senha, string senhaHash)
+        {
+            // Gerar o hash da senha digitada
+            string hashSenha = GerarHashSenha(senha);
+
+            // Comparar os hashes das senhas (ignorando diferenciação entre maiúsculas e minúsculas)
+            return StringComparer.OrdinalIgnoreCase.Compare(hashSenha, senhaHash) == 0;
+        }
+
+        static void Escolha() 
+        {
 
             Console.WriteLine("Qual operação deseja fazer: \n");
             Console.WriteLine("1 - Guardar dinheiro");
@@ -50,9 +123,7 @@ namespace projetoFinanceiro
 
             int operacao = int.Parse(Console.ReadLine());
 
-            Console.WriteLine("Digite quanto você ganha por mês: ");
-            double salarioMes = double.Parse(Console.ReadLine());
-
+            double salarioMes = 0;
             double resultado = 0;
             int prazo = 0;
             double valor = 0;
@@ -63,8 +134,14 @@ namespace projetoFinanceiro
             {
                 case 1:
                     {
+                        Console.WriteLine("Digite o prazo que você deseja guardar: ");
+                        prazo = int.Parse(Console.ReadLine());
+
+                        Console.WriteLine("Digite o valor que deseja guardar: ");
+                        valor = double.Parse(Console.ReadLine());
+
                         resultado = GuardarDinheiro(valor, prazo);
-                        Console.WriteLine("Você precisa quardar {0} por {1}meses para ter {2}",valor,prazo,resultado);
+                        Console.WriteLine("Você precisa guardar {0} por {1} meses para ter {2}",valor,prazo,resultado);
                         break;
                     }
                 case 2:
@@ -97,12 +174,6 @@ namespace projetoFinanceiro
 
         public static double GuardarDinheiro(double valor, double prazo)
         {
-            Console.WriteLine("Digite o prazo que você deseja guardar: ");
-            prazo = int.Parse(Console.ReadLine());
-
-            Console.WriteLine("Digite o valor que deseja guardar: ");
-            valor = double.Parse(Console.ReadLine());
-
             double resultado = valor * prazo;
             return resultado;
         }//Fim do método Soma
@@ -110,6 +181,9 @@ namespace projetoFinanceiro
         //Método Ver meu Saldo
         public static double VerMeuSaldo(double salarioMes, double divida)
         {
+            Console.WriteLine("Digite quanto você ganha por mês: ");
+            salarioMes = double.Parse(Console.ReadLine());
+
             Console.WriteLine("Digite o valor de suas dividas: ");
             divida = double.Parse(Console.ReadLine());
 
@@ -130,6 +204,9 @@ namespace projetoFinanceiro
         //Método Limite de gastos
         public static double LimiteDeGastos(double salarioMes, double limite)
         {
+            Console.WriteLine("Digite quanto você ganha por mês: ");
+            salarioMes = double.Parse(Console.ReadLine());
+
             Console.WriteLine("Digite o limite de gastos: ");
             limite = double.Parse(Console.ReadLine());
 
